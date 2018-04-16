ff8c88c324c15999aa6d6145a5cbebf0d4423a50
prajapatideepak2210 <prajapatideepak2210>
2018-04-16 19:26:35 +0530

    commit

--- angular app/controller/DashboardController.js
+++ angular app/controller/DashboardController.js
@@ -1,6 +1,20 @@
 
-app.controller('dashboardController', function($scope) {
+app.controller('dashboardController', function($scope,$mdDialog) {
 
   console.log("CONTROLLER")
   $scope.imagePath = 'assets/fwdphoneimages';
-});
+
+  $scope.status = '  ';
+  $scope.customFullscreen = false;
+// });
+$dialogs.showConfirmationDialog("Please confirm that you want to destroy your computer.", {
+	title: "Destroy your computer?",
+	buttonOkText: 'Yes, please do it!',
+	buttonCancelText : 'Maybe later',
+	callback: function(option){
+		if(option === "ok"){
+			alert("Installing Windows Vista...");
+		}
+	}
+})
+	});
