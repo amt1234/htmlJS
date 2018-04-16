a86031684cddc5bad2e2eb3cebb31290e80408a3
prajapatideepak2210 <prajapatideepak2210>
2018-04-16 19:30:17 +0530

    commit

--- angular app/controller/DashboardController.js
+++ angular app/controller/DashboardController.js
@@ -6,15 +6,4 @@ app.controller('dashboardController', function($scope,$mdDialog) {
 
   $scope.status = '  ';
   $scope.customFullscreen = false;
-// });
-$dialogs.showConfirmationDialog("Please confirm that you want to destroy your computer.", {
-	title: "Destroy your computer?",
-	buttonOkText: 'Yes, please do it!',
-	buttonCancelText : 'Maybe later',
-	callback: function(option){
-		if(option === "ok"){
-			alert("Installing Windows Vista...");
-		}
-	}
-})
-	});
+});
