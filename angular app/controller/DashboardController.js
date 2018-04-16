42fdab3c78652b20b8c4cc340cf5071b27483419
prajapatideepak2210 <prajapatideepak2210>
2018-04-16 18:58:30 +0530

    commit

--- angular app/controller/DashboardController.js
+++ angular app/controller/DashboardController.js
@@ -2,35 +2,5 @@
 app.controller('dashboardController', function($scope) {
 
   console.log("CONTROLLER")
-
-})
-app.filter('uniqueString',function(){
-  return function(items,keyword){
-    var filtered = [];
-
-    for(var i=0;i < items.length;i++){
-      var item = items[i];
-      var flag = false;
-
-      if(i==0){
-        filtered.push(item);
-      }else{
-        for(var j=0;j < filtered.length;j++){
-          var filteredItem = filtered[j];
-
-          if(item[keyword] == filteredItem[keyword]){
-            flag = true;
-          }
-          console.log(item[keyword]);
-        }
-
-        if(!flag){
-          filtered.push(item);
-        }
-      }
-
-    }
-
-    return filtered;
-  };
+  $scope.imagePath = 'assets/fwdphoneimages';
 });
--- angular app/controller/homeController.js
+++ angular app/controller/homeController.js
@@ -1,6 +1,7 @@
-app.controller('homeController', function($scope,$mdSidenav,jsonService) {
+app.controller('homeController', function($scope, $mdSidenav, jsonService,$state) {
   $scope.toggleLeft = buildToggler('left');
   $scope.toggleRight = buildToggler('right');
+
   function buildToggler(componentId) {
     return function() {
       $mdSidenav(componentId).toggle();
@@ -12,5 +13,38 @@ app.controller('homeController', function($scope,$mdSidenav,jsonService) {
     .then(function(response) {
       $scope.answer = response.data;
       console.log("$scope.answer", $scope.answer);
-    });
+    })
+    $scope.dashboard = function() {
+      $state.go('dashboard');
+    }
 });
+  app.filter('uniqueString', function() {
+    return function(items, keyword) {
+      var filtered = [];
+
+      for (var i = 0; i < items.length; i++) {
+        var item = items[i];
+        var flag = false;
+
+        if (i == 0) {
+          filtered.push(item);
+        } else {
+          for (var j = 0; j < filtered.length; j++) {
+            var filteredItem = filtered[j];
+
+            if (item.specs[keyword] == filteredItem.specs[keyword]) {
+              flag = true;
+            }
+            console.log(item.specs[keyword]);
+          }
+
+          if (!flag) {
+            filtered.push(item);
+          }
+        }
+
+      }
+
+      return filtered;
+    };
+  });
